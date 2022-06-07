718616c6482d10ac8f84e9f3c0a460905676a37d
Zakaria <zaktakk96@gmail.com>
2022-06-07 19:22:27 +0300

    Created "get users" function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controller
-{
-    //
+class UserController extends Controller{
+
+    public function getAllUsers($id = null){
+        if($id != null){
+            return response()->json([
+                "status" => "Success",
+                "user" => $id
+            ], 200);
+        }
+
+        return response()->json([
+            "status" => "Success",
+            "users" => [1,2,3]
+        ], 200);
+    }
+    
 }
